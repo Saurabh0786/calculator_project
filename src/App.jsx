73545b269dd0996b55a3f1a1fdd8f5f1cf3e8958
Saurabh0786 <sryder258@gmail.com>
2025-09-24 04:56:55 +0530

    Added the footer to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import SipCalculator from './components/SipCalculator';
-import MutualFundCalculator from './components/MutualFundCalculator';
-import EmiCalculator from './components/EmiCalculator';
-import FdCalculator from './components/FdCalculator';
-import { Container } from 'react-bootstrap';
+  import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+  import Header from './components/Header';
+  import Footer from './components/footer';
+  import SipCalculator from './components/SipCalculator';
+  import MutualFundCalculator from './components/MutualFundCalculator';
+  import EmiCalculator from './components/EmiCalculator';
+  import FdCalculator from './components/FdCalculator';
+  import { Container } from 'react-bootstrap';
 
-function App() {
-  return (
-    <Router>
-      <Header />
-      <Container className="mt-4">
-        <Routes>
-          <Route path="/" element={<SipCalculator />} />
-          <Route path="/mutual-fund" element={<MutualFundCalculator />} />
-          <Route path="/emi" element={<EmiCalculator />} />
-          <Route path="/fd" element={<FdCalculator />} />
-        </Routes>
-      </Container>
-    </Router>
-  );
-}
+  function App() {
+    return (
+      <Router>
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+          <Header />
+          <Container as="main" className="mt-4" style={{ flex: 1 }}>
+            <Routes>
+              <Route path="/" element={<SipCalculator />} />
+              <Route path="/mutual-fund" element={<MutualFundCalculator />} />
+              <Route path="/emi" element={<EmiCalculator />} />
+              <Route path="/fd" element={<FdCalculator />} />
+            </Routes>
+          </Container>
+          <Footer /> {/* Add the Footer at the end */}
+        </div>
+      </Router>
+    );
+  }
 
-export default App;
+  export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+ import React from 'react';
+
+  function Footer() {
+    return (
+      <footer className="app-footer">
+        <p>
+          Built with ❤️ by{' '}
+          <a
+            href="https://github.com/Saurabh0786"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Saurabh Shrivastav
+          </a>
+        </p>
+      </footer>
+    );
+  }
+
+  export default Footer;
